abecfa666bb99110a63419930782de6d4f58dccd
yy <1173960075@qq.com>
2019-04-02 20:10:02 +0800

    添加单元条件的repository

---
+++ cloud-service/cloud-sponsor/src/main/java/com/pyp/ad/repository/unit_condition/CreativeUnitRepository.java
@@ -0,0 +1,14 @@
+package com.pyp.ad.repository.unit_condition;
+
+import com.pyp.ad.entity.unit_condition.CreativeUnit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @description:
+ * @author: yy
+ * @data: Created in  2019/4/2 20:07
+ * @modifier:
+ * @version: V1.0
+ */
+public interface CreativeUnitRepository extends JpaRepository<CreativeUnit, Long> {
+}
---
+++ cloud-service/cloud-sponsor/src/main/java/com/pyp/ad/repository/unit_condition/UnitKeywordRepository.java
@@ -0,0 +1,14 @@
+package com.pyp.ad.repository.unit_condition;
+
+import com.pyp.ad.entity.unit_condition.UnitKeyword;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @description:
+ * @author: yy
+ * @data: Created in  2019/4/2 20:04
+ * @modifier:
+ * @version: V1.0
+ */
+public interface UnitKeywordRepository extends JpaRepository<UnitKeyword,Long> {
+}
